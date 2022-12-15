2a0a3eca7a3d7d881a051947e4b4251dbd49b37f
deepbhimani6 <deepbhimani6@gmail.com>
2022-12-15 10:15:44 +0530

    text change

--- src/App.js
+++ src/App.js
@@ -6,10 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <a>hello world</a>
+        <a>hello react js</a>
       </header>
     </div>
   );
 }
-
 export default App;
